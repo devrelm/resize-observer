7e81deae10ac292f1f3f506a821cfd18c476b563
Mike Deverell <mike@pelotoncycle.com>
2016-06-03 09:24:36 -0400

    add resize-observer.d.ts interface definitions

---
+++ src/resize-observer.d.ts
@@ -0,0 +1,25 @@
+export interface ResizeObserver {
+    constructor(callback: ResizeObserverCallback);
+    observe(target: Element): void;
+    unobserve(target: Element): void;
+    disconnect(): void;
+}
+]
+export interface ResizeObserverCallback {
+    (entries: ResizeObserverEntry[], observer: ResizeObserver): void;
+}
+
+export interface ResizeObserverEntry {
+    constructor(target: Element)
+    target: Element;
+    clientWidth: number;
+    clientHeight: number;
+}
+
+export interface ResizeObservation {
+    constructor(target: Element)
+    target: Element;
+    lastBroadcastWidth: number;
+    lastBroadcastHeight: number;
+    isActive(): boolean;
+}
